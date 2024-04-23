8b3de3170f3f1204f942bd9c995fb30e3bde1ae3
Mourya Shah <mshah3173@gmail.com>
2024-04-23 17:45:36 -0600

    Remove Firebase configuration and login page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,46 @@
+"use client";
+import { useState } from "react"; 
 import Link from "next/link";
-import React from "react";
+import { useUserAuth } from "./auth-context"; 
 
-export default function Home() {
+export default function Page() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  const handleSignIn = () => {
+    gitHubSignIn().catch(error => console.error("Error signing in:", error));
+  };
+
+  const handleSignOut = () => {
+    firebaseSignOut().catch(error => console.error("Error signing out:", error));
+  };
+
   return (
-    <Link href="/login">
-      <a>Login</a>
-    </Link>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+  <div className="bg-white rounded-lg shadow p-6">
+    {user ? (
+      <>
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">Login Here, {user.displayName}!</h2>
+        <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+          <button
+            onClick={handleSignOut}
+            className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded transition ease-in-out duration-150"
+          >
+            Sign Out
+          </button>
+        </div>
+      </>
+    ) : (
+      <div className="text-center">
+        <p className="text-lg text-gray-800 mb-4">Please sign in to access the weather information.</p>
+        <button
+          onClick={handleSignIn}
+          className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded transition ease-in-out duration-150"
+        >
+          Sign In with GitHub
+        </button>
+      </div>
+    )}
+  </div>
+</div>
   );
 }
